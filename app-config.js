df877f207da8050266e49e537c2d7c4349f1ac28
sid221722-collab <sid221722@gmail.com>
2026-09-05 19:08:44 +0300

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,3 @@
+window.AUDIT_PWA_CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyq7-oczRRvo7jylD_kFnjPVv1DIChANXdEmV-qWm7MDgx9vFkbz3gG5qDI5ltROHGjUA/exec'
+};
---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'audit-pwa-v34';
+const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./service-worker.js'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(APP_SHELL)).then(()=>self.skipWaiting())); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => { const copy=resp.clone(); caches.open(CACHE_NAME).then(c=>c.put(event.request,copy)); return resp; }).catch(()=>caches.match('./index.html'))));
+  }
+});
