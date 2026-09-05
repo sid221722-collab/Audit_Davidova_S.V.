07c36c1d7acb5161379fe69e54a8edd76ff3c824
sid221722-collab <sid221722@gmail.com>
2026-09-06 01:00:45 +0300

    Add files via upload

--- app-config.js
+++ app-config.js
@@ -1,4 +1,8 @@
-// URL Вашего Google Apps Script Web App
+// Конфигурация автономной PWA.
+// Поддерживаются оба имени переменной для совместимости с текущим index.html.
+window.AUDIT_PWA_CONFIG = {
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxjzCa3WCCoazrF3rRK3SzrOZWFBOaogz5hgm8sY_GtriJxOE-U1HWlMsGzbc5Do5JoTA/exec'
+};
 window.AUDIT_APP_CONFIG = {
-  GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzbo9mCuj6B6RyJ8edi-4A_P6MkPPbvPjQll1k9AENfN-CfYERlF2FVcWd8__q7d2tv-w/exec'
+  GOOGLE_SCRIPT_URL: window.AUDIT_PWA_CONFIG.APPS_SCRIPT_URL
 };
