d4fbf12e5e2786ed18c05b72828655db6311aa78
sid221722-collab <sid221722@gmail.com>
2026-09-06 15:26:32 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'audit-pwa-v46';
+const CACHE_NAME = 'audit-pwa-v51';
 const APP_SHELL = ['./', './index.html', './app-config.js', './manifest.webmanifest', './service-worker.js'];
 
 self.addEventListener('install', event => {
